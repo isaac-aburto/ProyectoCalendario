e2c00313bbe54efcc84a19c91dc6db2909327317
isaac-aburto <73562356+isaac-aburto@users.noreply.github.com>
2023-05-29 22:12:03 -0400

    Agregar archivos de proyecto.

---
+++ ProyectoCalendario/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using ProyectoCalendario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace ProyectoCalendario.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (ModeloCalendarioEntities1 conexion = new ModeloCalendarioEntities1()) {
+     
+                //var nombre = conexion.Usuarios.Where(w => w.nombre == "Isaac")
+                //    .Select(s => s.nombre)
+                //    .FirstOrDefault();
+                //ViewData["nombreUsuario"] = nombre;
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
